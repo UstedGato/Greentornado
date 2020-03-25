4c281333587c8480779a35e30ed70b1d3a442c51
SilvershotsHQ <62627101+SilvershotsHQ@users.noreply.github.com>
2020-03-24 20:48:30 -0400

    asd

--- commands/test/command.js
+++ commands/test/command.js
@@ -3,7 +3,7 @@ const { Command } = require('discord.js-commando');
 module.exports = class ReplyCommand extends Command {
     constructor(client) {
         super(client, {
-            name: 'reeee',
+            name: 'ree',
             group: 'test',
             memberName: 'reply',
             description: 'Replies with a Message.',
