b9591dbb39d58f5597a96d1d70d8b8791499caf4
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-03-25 22:30:52 -0400

    bdgf

---
+++ commands/coins/coins.js
@@ -0,0 +1,61 @@
+const { Command } = require('discord.js-commando');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+module.exports = class ReplyCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'coins',
+            group: 'coins',
+            memberName: 'coins',
+            description: 'Check your coins.',
+            examples: ['repeat'],
+            
+            args: [
+				{
+					key: 'user',
+					label: 'user',
+					type: 'string'
+				}
+			]
+        });
+    }
+
+    async run(msg, { user }){
+        const doc = new GoogleSpreadsheet(process.env.SHEET);
+        await doc.useServiceAccountAuth({
+            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY,
+          });
+        await doc.loadInfo();
+        const sheet = doc.sheetsByIndex[0];
+        var rows = await sheet.getRows();
+        var msguserid = msg.author.id;
+        
+        if (user != "") {
+            for (i = 0; i < rows.length; i++) {
+                if (rows[i].id === msguserid) {
+                    //var rownum = i;
+                    var rowy = 1;
+                    break;
+                }
+            }
+            if (rowy === 1) {
+                return msg.reply("You have " + rows[i].coins + " coins.");
+            } else {
+                return msg.reply("You're broke, idiot.");
+            }
+        } else {
+            for (i = 0; i < rows.length; i++) {
+                if (rows[i].id === user) {
+                    //var rownum = i;
+                    var rowy = 1;
+                    break;
+                }
+            }
+            if (rowy === 1) {
+                return msg.reply("They have " + rows[i].coins + " coins.");
+            } else {
+                return msg.reply("They're broke, you idiot.");
+            }
+        }
+    }
+};
