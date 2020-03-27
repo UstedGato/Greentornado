ce365ba81216b8fc85588ff6af45a93d4ca9ce8c
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-03-27 17:33:18 -0400

    tyguhj

---
+++ commands/annoyance/elias.js
@@ -0,0 +1,30 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class ReplyCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'elias',
+            group: 'annoyance',
+            memberName: 'elias',
+            description: 'Punish Elias.',
+            examples: ['reply'],
+            args: [
+				{
+					key: 'length',
+                    label: 'length',
+                    prompt: 'ghjfghg',
+                    type: 'string',
+                    //default: ''
+				}
+			]
+        });
+    }
+
+    run(msg) {
+        for (i = 0; i < "args.length"; i++) {
+            msg.say("@SilvershotsHQ#9454");
+            msg.say("@SilvershotsHQ#9454");
+        }
+        return msg.say('SilvershotsHQ#9454');
+    }
+};
