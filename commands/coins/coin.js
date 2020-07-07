1fb9c658a074cb447d34ccc9c917ee818268cfec
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-07-07 14:53:20 -0400

    tokens but again

--- commands/coins/coin.js
+++ commands/coins/coin.js
@@ -25,7 +25,7 @@ module.exports = class ReplyCommand extends Command {
         const doc = new GoogleSpreadsheet(process.env.SHEET);
         await doc.useServiceAccountAuth({
             client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
           });
         await doc.loadInfo();
         const sheet = doc.sheetsByIndex[0];
