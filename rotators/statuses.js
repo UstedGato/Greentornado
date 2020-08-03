5c9bf082cf79236d5ff5909a132d6efb15e31b46
potatocloudy <69132234+potatocloudy@users.noreply.github.com>
2020-08-03 13:08:14 -0400

    hi

--- rotators/statuses.js
+++ rotators/statuses.js
@@ -2,7 +2,10 @@ var statuses = [
     { activity: { name: 'the beans' , type: "WATCHING"}},
     { activity: { name: 'kirby with a gun', type: "PLAYING"}},
     { activity: { name: 'shrek 2', type: "STREAMING"}},
-    { activity: { name: 'megalovania', type: "LISTENING"}}
+    { activity: { name: 'megalovania', type: "LISTENING"}},
+    { activity: { name: 'family guy online', type: "PLAYING"}},
+    { activity: { name: 'the screams of the underworld', type: "LISTENING"}},
+    { activity: { name: 'minecraft mod reviews', type: "STREAMING"}}
 ]
 
 
