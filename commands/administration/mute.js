bfeb3eced84d7bc81974aebe3c95b3a2378376b7
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-08-07 00:56:30 -0400

    make mute look nice

--- commands/administration/mute.js
+++ commands/administration/mute.js
@@ -1,9 +1,7 @@
 const { Command } = require('discord.js-commando');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 async function unmute(member, role, msg) {
-    console.log(member + role + msg);
     await member.roles.remove(role.id);
-    await msg.channel.send(`${member.user}` + ' has now been unmuted.')
 }
 module.exports = class ReplyCommand extends Command {
     constructor(client) {
@@ -37,7 +35,16 @@ module.exports = class ReplyCommand extends Command {
         let role = msg.guild.roles.cache.find(role => role.name === "Muted");
         var member  = msg.guild.member(msg.mentions.users.first() || msg.guild.members.cache.get(args[1]));
         await member.roles.add(role.id);
-        await msg.reply('muted');
+        const embed = {
+        "title": "Muted x for 24min",
+        "color": 15350333,
+        "author": {
+            "name": "Mute",
+            "url": "https://discordapp.com",
+            "icon_url": "https://aagaming-public.s3.us-west-000.backblazeb2.com/baseline_volume_off_white_18dp.png"
+        }
+        };
+        await msg.reply({ embed });
         setTimeout(unmute, amount * 60 * 1000, member, role, msg);
     }
 };
