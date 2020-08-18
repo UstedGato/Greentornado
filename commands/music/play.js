f489810ee5c5b1fd1362f8b4c976ebb2b0c79e47
AAGithub <aa@bigdumb.gq>
2020-08-18 16:59:03 -0400

    queue system

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,63 +1,45 @@
 const commando = require('discord.js-commando');
-const ytdl = require('ytdl-core');
-const fetch = require('node-fetch');
-const querystring = require('querystring');
+const { Player, GuildQueue, getQueues } = require('./../../utils/musicClasses');
 
 module.exports = class ReplyCommand extends commando.Command {
   constructor(client) {
     super(client, {
       name: 'play',
-      aliases: ['play youtube.com/video'],
+      aliases: ['play youtube.com/url'],
       group: 'music',
       memberName: 'play',
-      description: 'Music player form youtube.',
+      description: 'Music player.',
                   args: [
 				{
-					key: 'video',
-					label: 'video',
-					prompt: 'No video was specified',
+					key: 'url',
+					label: 'url',
+					prompt: 'No url was specified',
                     type: 'string'
 				}
 			]
     });
   }
 
-  async run(message, { video }) {
+  async run(message, { url }) {
     if (message.channel.type === 'dm') return;
-
+    if (message.content.indexOf('youtu') === -1 && message.content.indexOf('soundcloud') === -1){
+      return message.reply('Invalid link.');
+    }
     const voiceChannel = message.member.voice.channel;
 
     if (!voiceChannel) {
-        return message.reply('please join a voice channel first!');
+        return message.reply('Please join a voice channel first!');
     }
-    let stream;
-    let connection = await voiceChannel.join();
-      if (message.content.indexOf('youtu') !== -1){
-        stream = ytdl(video, { filter: 'audioonly' });
-      }
-      message.react('✅')
-      if (message.content.indexOf('soundcloud') !== -1) {
-        const resolve = await fetch(`https://api-v2.soundcloud.com/resolve?url=${video}&client_id=${process.env.SOUNDCLOUD_CLIENT_ID}`);
-        const resolveResponse = await resolve.json();
-        let streamApiUrl;
-        for (let i = 0; i < resolveResponse.media.transcodings.length; i++) {
-          if (resolveResponse.media.transcodings[i].format.protocol === "progressive") {
-            streamApiUrl = resolveResponse.media.transcodings[i].url;
-            break;
-          }
-        } 
+    const player = new Player(url, message);
+    let queues = getQueues();
+    if (queues[message.guild.id]) {
+      queues[message.guild.id].addPlayer(player);
+    }
+    else {
+      const queue = new GuildQueue(voiceChannel, message.guild);
+      await queue.addPlayer(player);
+      await queue.init();
+    }
 
-        const streamUrl = await fetch(`${streamApiUrl}?client_id=${process.env.SOUNDCLOUD_CLIENT_ID}`);
-        const streamUrlResponse = await streamUrl.json();
-        const response = await fetch(`${streamUrlResponse.url}`);
-
-        if (response.ok) {
-          stream = response.body
-        }
-
-        }
-        const dispatcher = connection.play(stream);
-
-        dispatcher.on('finish', () => voiceChannel.leave());
-      }
+  }
 };
--- utils/musicClasses.js
+++ utils/musicClasses.js
@@ -1,7 +1,80 @@
-module.exports += class Rectangle {
-  constructor(URL, service) {
-    this.URL = URL;
-    this.service = service;
-  }
-}
+const ytdl = require('ytdl-core');
+const fetch = require('node-fetch');
+let queues = new Object();
 
+module.exports.Player = class Player {
+  constructor(URL, message) {
+    this.URL = URL;
+    this.message = message;
+  }
+  async play() {
+    try {
+    let stream;
+    if (this.message.content.indexOf('youtu') !== -1){
+      stream = ytdl(this.URL, { filter: 'audioonly' });
+    }
+    if (this.message.content.indexOf('soundcloud') !== -1) {
+      const resolve = await fetch(`https://api-v2.soundcloud.com/resolve?url=${this.URL}&client_id=${process.env.SOUNDCLOUD_CLIENT_ID}`);
+      const resolveResponse = await resolve.json();
+      let streamApiUrl;
+      for (let i = 0; i < resolveResponse.media.transcodings.length; i++) {
+        if (resolveResponse.media.transcodings[i].format.protocol === "progressive") {
+          streamApiUrl = resolveResponse.media.transcodings[i].url;
+          break;
+        }
+      } 
+
+      const streamUrl = await fetch(`${streamApiUrl}?client_id=${process.env.SOUNDCLOUD_CLIENT_ID}`);
+      const streamUrlResponse = await streamUrl.json();
+      const response = await fetch(`${streamUrlResponse.url}`);
+
+      if (response.ok) {
+        stream = response.body;
+      }
+
+      }
+      this.message.react('✅');
+      this.dispatcher = this.connection.play(stream);
+
+      this.dispatcher.on('finish', () => this.guildQueue());
+    } catch (error) {
+      this.message.reply(`error playing \`\`${this.URL}: ${error}`);
+    }
+    }
+};
+
+module.exports.GuildQueue = class GuildQueue {
+  constructor(voiceChannel, guild) {
+    this.voiceChannel = voiceChannel;
+    this.guild = guild;
+    this.players = [];
+  }
+  async init() {
+    this.connection = await this.voiceChannel.join();
+    queues[this.guild.id] = this;
+    this.players[0].connection = this.connection;
+    this.players[0].guild = this.guild;
+    this.players[0].guildQueue = this.next;
+    await this.players[0].play();
+  }
+  async next() {
+    queues[this.guild.id].players.shift();
+    if (queues[this.guild.id].players[0]) {
+      queues[this.guild.id].players[0].connection = queues[this.guild.id].connection;
+      queues[this.guild.id].players[0].guild = queues[this.guild.id].guild;
+      queues[this.guild.id].players[0].guildQueue = queues[this.guild.id].next;
+      await queues[this.guild.id].players[0].play();
+    }
+    else {
+      delete queues[this.guild.id];
+      this.connection.channel.leave();
+    }
+  }
+
+  async addPlayer(player) {
+    this.players.push(player);
+  }
+};
+module.exports.getQueues = function() {
+  return queues;
+};
