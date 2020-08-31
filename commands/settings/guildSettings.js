7a33e5c6a1ed34ec4d9df95e9b2a34bde71e527b
ADoesGit <aa@bigdumb.gq>
2020-08-31 14:50:56 -0400

    some settings progress yes

--- commands/settings/guildSettings.js
+++ commands/settings/guildSettings.js
@@ -1,4 +1,8 @@
 const { Command } = require('discord.js-commando');
+const faunadb = require('faunadb'),
+  q = faunadb.query,
+  client = new faunadb.Client({ secret: process.env.FAUNA_KEY })
+
 module.exports = class ReplyCommand extends Command {
     constructor(client) {
         super(client, {
@@ -6,11 +10,59 @@ module.exports = class ReplyCommand extends Command {
             group: 'settings',
             memberName: 'npm',
             description: 'opens guild settings.',
-            examples: ['g!guildsettings']
+            examples: ['g!guildsettings'],
+            userPermissions: ['ADMINISTRATOR'],
         });
     }
-
+    async getSettings(id) {
+        let settings;
+        try {
+            settings = await client.query(
+                q.Get(
+                  q.Match(
+                      q.Index("guildIndex"),
+                      id
+                    )
+                )
+            ) 
+            } catch (error) {
+                await client.query(
+                    q.Create(
+                      q.Collection('guildSettings'),
+                      { data: { id: id } },
+                    )
+                  )
+                return true
+            }
+        return settings
+    }
     async run(msg){
-        return msg.reply('<@373833473091436546> fix ur stupid settings already');
+        const embed = {
+            "title": "Server Name",
+            "description": "React with one of theese reactions to change it's setting",
+            "color": 9174784,
+            "thumbnail": {
+              "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+            },
+            "image": {
+              "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+            },
+            "author": {
+              "name": "Guild Settings",
+              "url": "https://discordapp.com",
+              "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+            },
+            "fields": [
+              {
+                "name": ":hash:",
+                "value": "Welcome channel:"
+              },
+              {
+                "name": "🌃",
+                "value": "Welcome background:"
+              }
+            ]
+          };
+        const botMessage = await message.channel.send({ embed })
     }
 };
