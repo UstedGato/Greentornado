b1e2173e9fab82e7305c8e190f9145db372ffab3
potatocloudy <69132234+potatocloudy@users.noreply.github.com>
2020-09-02 14:01:51 -0400

    oh no

--- rotators/statuses.js
+++ rotators/statuses.js
@@ -7,7 +7,13 @@ const statuses = [
     { activity: { name: 'the screams of the underworld', type: "LISTENING"}},
     { activity: { name: 'minecraft mod reviews', type: "STREAMING"}},
     { activity: { name: 'the corn cube', type: "WATCHING"}},
-    { activity: { name: 'grus moon tycoon', type: "PLAYING"}}
+    { activity: { name: 'grus moon tycoon', type: "PLAYING"}},
+    { activity: { name: 'gay bowser 10 hours', type: "LISTENING"}},
+    { activity: { name: 'roblox', type: "STREAMING"}},
+    { activity: { name: 'with fire', type: "PLAYING"}},
+    { activity: { name: 'AHHHHHHHHHHHHHHHH', type: "LISTENING"}},
+    { activity: { name: 'shaggys revenge', type: "PLAYING"}},
+    { activity: { name: 'despacito (minecraft cover)', type: "LISTENING"}}
 ]
 
 
