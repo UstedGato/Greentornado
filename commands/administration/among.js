4d44ec88fd9161a8e2b735b78022a596fa2ee5e8
AAGaming <aa@bigdumb.gq>
2020-09-13 21:13:02 -0400

    among us command

---
+++ commands/administration/among.js
@@ -0,0 +1,75 @@
+const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const AudioMixer = require('audio-mixer');
+let yes = false;
+let deadconnection;
+let aliveconnection;
+let mixer = null;
+let users = [];
+let inputs = [];
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+async function unmute(member, role, msg) {
+    await member.roles.remove(role.id);
+}
+module.exports = class ReplyCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'among',
+            group: 'administration',
+            memberName: 'amongus',
+            description: 'Mute a user(in minutes).',
+            examples: ['g!among'],
+            userPermissions: ['ADMINISTRATOR']
+        });
+    }
+
+    async run(msg, { amount, user }) {
+        if (yes === false) {
+            client.login(process.env.BRIDGE_TOKEN)
+            const guildthroughotherclient = await client.guilds.resolveID(msg.guild.id)
+            const alivechannel = msg.guild.channels.resolveID(process.env.ALIVE_AMONG_CHANNEL)
+            const deadchannel = guildthroughotherclient.channels.resolveID(process.env.DEAD_AMONG_CHANNEL)
+            deadconnection = await deadchannel.join()
+            aliveconnection = await alivechannel.join()
+            const receiver = aliveconnection.createReceiver();
+            mixer = new AudioMixer.Mixer({
+                channels: 1,
+                bitDepth: 16,
+                sampleRate: 48000,
+                clearInterval: 250
+            });
+            aliveconnection.on('speaking', (user, speaking) => {
+              if (speaking) {
+                console.log(`I'm listening to ${user}`);
+                // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
+                users[user.id] = receiver.createPCMStream(user);
+                inputs[user.id] = mixer.input({
+                    channels: 1,
+                    volume: 100
+                });
+                users[user.id].pipe(inputs[user.id]);
+                // when the stream ends (the user stopped talking) tell the user
+                users[user.id].on('end', () => {
+                    mixer.removeInput(inputs[user.id])
+                });
+              } else {
+
+              }
+            });
+            let connection = new ReadableStream
+            mixer.pipe(connection)
+            deadchannel.play(connection)
+        } else {
+            aliveconnection.disconnect()
+            deadconnection.disconnect()
+            client.destroy()
+            users = []
+            inputs = []
+            mixer = null
+        }
+    }
+};
