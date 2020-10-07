c59af9d02e22538dcac59e57ea7abbad0b737236
AA <aa@bigdumb.gq>
2020-10-06 22:33:54 -0400

    moar things

--- rest/endpoints/badge.js
+++ rest/endpoints/badge.js
@@ -44,6 +44,9 @@ module.exports = (app, client) => {
 </defs>
 </svg>
         `
+        res.setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.setHeader('Expires', '-1');
+        res.setHeader('Pragma', 'no-cache');
         res.setHeader('content-type', 'image/svg+xml')
         res.send(svg)
     })
